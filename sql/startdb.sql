2a7a00a8ac6434181ecfbc9533437063f767ca16
Pedro <tigreped@gmail.com>
2022-03-24 15:43:42 -0300

    Alterações para que os campos não fiquem como bigint.

--- sql/startdb.sql
+++ sql/startdb.sql
@@ -7,9 +7,9 @@
 drop table if exists empresa;
 create table empresa (
 	-- NÚMERO BASE DE INSCRIÇÃO NO CNPJ (OITO PRIMEIROS DÍGITOS DO CNPJ).
-	cnpj TEXT PRIMARY KEY,
+	cnpj VARCHAR(15) PRIMARY KEY,
 	-- NOME EMPRESARIAL DA PESSOA JURÍDICA
-	razao_social TEXT,
+	razao_social VARCHAR,
 	-- CÓDIGO DA NATUREZA JURÍDICA
 	codigo_natureza_juridica INT,
 	-- QUALIFICAÇÃO DA PESSOA FÍSICA RESPONSÁVEL PELA EMPRESA
@@ -24,26 +24,26 @@ create table empresa (
 	porte INT,
 	-- O ENTE FEDERATIVO RESPONSÁVEL É PREENCHIDO PARA OS CASOS DE ÓRGÃOS E ENTIDADES DO GRUPO DE NATUREZA JURÍDICA 1XXX.
 	-- PARA AS DEMAIS NATUREZAS, ESTE ATRIBUTO FICA EM BRANCO.
-	ente_federativo_responsavel TEXT
+	ente_federativo_responsavel VARCHAR
 );
 
 -- Gerenciar as operações relativas aos Estabelecimentos (CNPJ)
 drop table if exists estabelecimento;
 create table estabelecimento (
 	-- Chave primária
-	cnpj TEXT,
+	cnpj VARCHAR(15),
 	-- NÚMERO BASE DE INSCRIÇÃO NO CNPJ (OITO PRIMEIROS DÍGITOS DO CNPJ).
-	cnpj_basico TEXT,
+	cnpj_basico VARCHAR(8),
 	-- NÚMERO DO ESTABELECIMENTO DE INSCRIÇÃO NO CNPJ (DO NONO ATÉ O DÉCIMO SEGUNDO DÍGITO DO CNPJ).
-	cnpj_ordem TEXT,
+	cnpj_ordem VARCHAR(4),
 	-- DÍGITO VERIFICADOR DO NÚMERO DE INSCRIÇÃO NO CNPJ (DOIS ÚLTIMOS DÍGITOS DO CNPJ).
-	cnpj_dv INT,
+	cnpj_dv VARCHAR(2),
 	-- CÓDIGO DO IDENTIFICADOR MATRIZ/FILIAL:
 		-- 1 - MATRIZ
 		-- 2 - FILIAL
 	identificador_matriz_filial INT,
 	-- CORRESPONDE AO NOME FANTASIA
-	nome_fantasia TEXT,
+	nome_fantasia VARCHAR,
 	-- CÓDIGO DA SITUAÇÃO CADASTRAL:
 		-- 01 - NULA
 		-- 2 - ATIVA
@@ -52,35 +52,35 @@ create table estabelecimento (
 		-- 08 - BAIXADA
 	situacao_cadastral INT,
 	-- DATA DO EVENTO DA SITUAÇÃO CADASTRAL
-	data_situacao_cadastral TEXT,
+	data_situacao_cadastral VARCHAR(10),
 	-- CÓDIGO DO MOTIVO DA SITUAÇÃO CADASTRAL
 	motivo_situacao_cadastral INT,
 	-- NOME DA CIDADE NO EXTERIOR
-	nome_cidade_exterior TEXT,
+	nome_cidade_exterior VARCHAR,
 	-- CÓDIGO DO PAIS
 	pais INT,
 	-- DATA DE INÍCIO DA ATIVIDADE
-	data_inicio_atividade TEXT,
+	data_inicio_atividade VARCHAR(10),
 	-- CÓDIGO DA ATIVIDADE ECONÔMICA PRINCIPAL DO ESTABELECIMENTO
 	cnae_fiscal INT,
 	-- CÓDIGO DA(S) ATIVIDADE(S) ECONÔMICA(S) SECUNDÁRIA(S) DO ESTABELECIMENTO
 	cnae_fiscal_secundario INT,
 	-- DESCRIÇÃO DO TIPO DE LOGRADOURO
-	tipo_logradouro TEXT,
+	tipo_logradouro VARCHAR,
 	-- NOME DO LOGRADOURO ONDE SE LOCALIZA O ESTABELECIMENTO.
-	logradouro TEXT,
+	logradouro VARCHAR,
 	-- NÚMERO ONDE SE LOCALIZA O ESTABELECIMENTO. QUANDO NÃO HOUVER PREENCHIMENTO DO NÚMERO HAVERÁ ‘S/N’.
-	numero TEXT,
+	numero VARCHAR,
 	-- COMPLEMENTO PARA O ENDEREÇO DE LOCALIZAÇÃO DO ESTABELECIMENTO
-	complemento TEXT,
+	complemento VARCHAR,
 	-- BAIRRO ONDE SE LOCALIZA O ESTABELECIMENTO.
-	bairro TEXT,
+	bairro VARCHAR,
 	-- CÓDIGO DE ENDEREÇAMENTO POSTAL REFERENTE AO LOGRADOURO NO QUAL O ESTABELECIMENTO ESTA LOCALIZADO
 	cep INT,
 	-- SIGLA DA UNIDADE DA FEDERAÇÃO EM QUE SE ENCONTRA O ESTABELECIMENTO
-	uf TEXT,
+	uf VARCHAR,
 	-- CÓDIGO DO MUNICÍPIO DE JURISDIÇÃO ONDE SE ENCONTRA O ESTABELECIMENTO
-	municipio TEXT,
+	municipio VARCHAR,
 	-- CONTÉM O DDD 1
 	ddd_1 VARCHAR(2),
 	-- CONTÉM O NÚMERO DO TELEFONE 1
@@ -94,43 +94,43 @@ create table estabelecimento (
 	-- CONTÉM O NÚMERO DO FAX
 	telefone_fax VARCHAR(9),
 	-- CONTÉM O E-MAIL DO CONTRIBUINTE
-	correio_eletronico TEXT,
+	correio_eletronico VARCHAR,
 	-- SITUAÇÃO ESPECIAL DA EMPRESA
-	situacao_especial TEXT,
+	situacao_especial VARCHAR,
 	-- DATA EM QUE A EMPRESA ENTROU EM SITUAÇÃO ESPECIAL
-	data_situacao_especial TEXT
+	data_situacao_especial VARCHAR(10)
 );
 
 -- Gerenciar os Dados do Simples Nacional
 drop table if exists simples;
 create table simples (
 	-- NÚMERO BASE DE INSCRIÇÃO NO CNPJ (OITO PRIMEIROS DÍGITOS DO CNPJ).
-	cnpj_basico TEXT,
+	cnpj_basico VARCHAR(8),
 	-- INDICADOR DA EXISTÊNCIA DA OPÇÃO PELO SIMPLES.
 		-- S - SIM
 		-- N - NÃO
 		-- EM BRANCO - OUTROS
-	opcao_pelo_simples VARCHAR(1),
+	opcao_pelo_simples CHAR,
 	-- DATA DE OPÇÃO PELO SIMPLES
-	data_opcao_pelo_simples TEXT,
+	data_opcao_pelo_simples VARCHAR(10),
 	-- DATA DE EXCLUSÃO DO SIMPLES
-	data_exclusao_pelo_simples TEXT,
+	data_exclusao_pelo_simples VARCHAR(10),
 	-- INDICADOR DA EXISTÊNCIA DA OPÇÃO PELO MEI
 		-- S - SIM
 		-- N - NÃO
 		-- EM BRANCO - OUTROS
-	opcao_pelo_mei VARCHAR(1),
+	opcao_pelo_mei CHAR,
 	-- DATA DE OPÇÃO PELO MEI
-	data_opcao_pelo_mei TEXT,
+	data_opcao_pelo_mei VARCHAR(10),
 	-- DATA DE EXCLUSÃO DO MEI
-	data_exclusao_pelo_mei TEXT
+	data_exclusao_pelo_mei VARCHAR(10)
 );
 
 -- Gerenciar as operações relativas a Sócios
 drop table if exists socio;
 create table socio (
 	-- NÚMERO BASE DE INSCRIÇÃO NO CNPJ (CADASTRO NACIONAL DA PESSOA JURÍDICA).
-	cnpj_basico TEXT,
+	cnpj_basico VARCHAR(8),
 	-- CÓDIGO DO IDENTIFICADOR DE SÓCIO
 		-- 1 - PESSOA JURÍDICA
 		-- 2 - PESSOA FÍSICA
@@ -138,19 +138,19 @@ create table socio (
 	identificador_socio INT,
 	-- NOME DO SÓCIO PESSOA FÍSICA OU A RAZÃO SOCIAL E/OU NOME EMPRESARIAL DA PESSOA JURÍDICA
 	-- E/OU NOME DO SÓCIO /RAZÃO SOCIAL DO SÓCIO ESTRANGEIRO
-	razao_social TEXT,
+	razao_social VARCHAR,
 	-- CPF OU CNPJ DO SÓCIO (SÓCIO ESTRANGEIRO NÃO TEM ESTA INFORMAÇÃO).
-	cnpj_cpf_socio TEXT,
+	cnpj_cpf_socio VARCHAR(15),
 	-- CÓDIGO DA QUALIFICAÇÃO DO SÓCIO
 	codigo_qualificacao_socio INT,
 	-- DATA DE ENTRADA NA SOCIEDADE
-	data_entrada_sociedade TEXT,
+	data_entrada_sociedade VARCHAR(10),
 	-- CÓDIGO PAÍS DO SÓCIO ESTRANGEIRO
 	codigo_pais_socio_estrangeiro INT,
 	-- NÚMERO DO CPF DO REPRESENTANTE LEGAL
 	numero_cpf_representante_legal INT,
 	-- NOME DO REPRESENTANTE LEGAL
-	nome_representante_legal TEXT,
+	nome_representante_legal VARCHAR,
 	-- CÓDIGO DA QUALIFICAÇÃO DO REPRESENTANTE LEGAL
 	codigo_qualificacao_representante_legal INT,
 	-- CÓDIGO CORRESPONDENTE À FAIXA ETÁRIA DO SÓCIO
@@ -164,7 +164,7 @@ create table socio (
 		-- 7 para os intervalos entre 61 a 70 anos
 		-- 8 para os intervalos entre 71 a 80 anos
 		-- 9 para maiores de 80 anos.
-	faixa_etaria INT
+	faixa_etaria INT(1)
 );
 
 -----------------------------------------------------------------------------------------------------------------
@@ -176,7 +176,7 @@ create table pais (
 	-- CÓDIGO DO PAÍS
 	codigo INT,
 	-- NOME DO PAÍS
-	descricao TEXT
+	descricao VARCHAR
 );
 
 -- Gerenciar as operações relativas aos países
@@ -185,7 +185,7 @@ create table municipio (
 	-- CÓDIGO DO MUNICÍPIO
 	codigo INT,
 	-- NOME DO MUNICÍPIO
-	descricao TEXT
+	descricao VARCHAR
 );
 
 -- Gerenciar as operações relativas às qualificações dos sócios
@@ -194,7 +194,7 @@ create table qualificacao_socio (
 	-- CÓDIGO DA QUALIFICAÇÃO DO SÓCIO
 	codigo INT,
 	-- NOME DA QUALIFICAÇÃO DO SÓCIO
-	descricao TEXT
+	descricao VARCHAR
 );
 
 -- Gerenciar as operações relativas às naturezas jurídicas
@@ -203,7 +203,7 @@ create table natureza_juridica (
 	-- CÓDIGO DA NATUREZA JURÍDICA
 	codigo INT,
 	-- NOME DA NATUREZA JURÍDICA
-	descricao TEXT
+	descricao VARCHAR
 );
 
 -- Gerenciar as operações relativas às Empresas (CNAE)
@@ -212,10 +212,10 @@ create table cnae (
 	-- CÓDIGO DA ATIVIDADE ECONÔMICA
 	codigo INT,
 	-- NOME DA ATIVIDADE ECONÔMICA
-	descricao TEXT
+	descricao VARCHAR
 );
 
--- Primary keys:  
+-- Primary keys:
 
 ALTER TABLE IF EXISTS empresa DROP CONSTRAINT IF EXISTS pk_empresa_id;
 ALTER TABLE empresa ADD CONSTRAINT pk_empresa_id PRIMARY KEY (cnpj);
@@ -242,13 +242,3 @@ CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_estabelecimento_cnae_fiscal ON estab
 CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_estabelecimento_local ON estabelecimento (uf, municipio, bairro);
 CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_estabelecimento_situacao_cadastral ON estabelecimento (situacao_cadastral);
 CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_simples_mei_simples ON simples (opcao_pelo_mei, opcao_pelo_simples);
-
--- #Review and adjust with or without usage of pg_trgm!
---CREATE EXTENSION IF NOT EXISTS pg_trgm;  -- PostgreSQL-only
---CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_trgm_socio_nome_socio ON socio USING gin (nome_socio gin_trgm_ops);
---CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_trgm_empresa_razao_social ON empresa USING gin (razao_social gin_trgm_ops);
---CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_trgm_empresa_nome_fantasia ON empresa USING gin (nome_fantasia gin_trgm_ops);
---CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_trgm_cnae_descricao ON cnae USING gin (descricao_subclasse gin_trgm_ops);
---CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_trgm_empresa_uf ON empresa USING gin (uf gin_trgm_ops);
---CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_trgm_empresa_municipio ON empresa USING gin (municipio gin_trgm_ops);
---CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_trgm_empresa_bairro ON empresa USING gin (bairro gin_trgm_ops);
